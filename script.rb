8d39fb55029203efc7f9095b10ad6ba19fa0b88f
Aleksander <51536161+akozdev@users.noreply.github.com>
2020-02-11 09:59:09 +0100

    Fix intendation problems

--- script.rb
+++ script.rb
@@ -1,6 +1,6 @@
 def camelize(str)
   # Removes spaces, underscores and hyphens, and capitalizes the first letter of each word
-	str.split(/ |\_|\-/).map(&:capitalize).join()
+  str.split(/ |\_|\-/).map(&:capitalize).join()
 end
 
 # Path to the directory with images
@@ -25,14 +25,14 @@ export_string = "";
 # For each .png file, create the export string,
 # for example, "export { default as myInfo } from './images/myInfo.png';"
 file_names.each do |file_name|
-	# Get the name of the file and the extension separately
-	file_name_without_ext = file_name.split(".")[0]
+  # Get the name of the file and the extension separately
+  file_name_without_ext = file_name.split(".")[0]
 	file_ext = file_name.split(".")[1];
+	
+  # Skip non-png files, and the files with @ in the name
+  next if file_ext != FILE_TYPE || file_name_without_ext.include?("@")
 
-	# Skip non-png files, and the files with @ in the name
-	next if file_ext != FILE_TYPE || file_name_without_ext.include?("@")
-
-	# Make the camel case version of file names
+  # Make the camel case version of file names
   file_name_camel_case = camelize(file_name_without_ext)
 
   # Construct the export string
