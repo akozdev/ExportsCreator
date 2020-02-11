34f85b9bf83af9f3274789eb0e15aabe97687709
Aleksander <51536161+akozdev@users.noreply.github.com>
2020-02-11 09:56:09 +0100

    Remove hardcoded values with variables

--- script.rb
+++ script.rb
@@ -8,7 +8,13 @@ PATH = "./images";
 
 # Specify relative path in the export string, 
 # for example, "./images" in "export { default as myInfo } from './images/myInfo.png';"
-path_in_export_string = "./images"
+PATH_IN_EXPORT_STRING = "./images"
+
+# Specify the type of files to be exported
+FILE_TYPE = 'png'
+
+# Specify the name of the file where exports will be saved
+FILE_NAME = 'index.ts'
 
 # Get the names of the files in the specified directory
 file_names = Dir.entries(PATH).drop(2)
@@ -24,13 +30,13 @@ file_names.each do |file_name|
 	file_ext = file_name.split(".")[1];
 
 	# Skip non-png files, and the files with @ in the name
-	next if file_ext != "png" || file_name_without_ext.include?("@")
+	next if file_ext != FILE_TYPE || file_name_without_ext.include?("@")
 
 	# Make the camel case version of file names
   file_name_camel_case = camelize(file_name_without_ext)
 
   # Construct the export string
-  export_string += "export { default as #{file_name_camel_case} } from '#{path_in_export_string}/#{file_name}';\n"
+  export_string += "export { default as #{file_name_camel_case} } from '#{PATH_IN_EXPORT_STRING}/#{file_name}';\n"
 end
 
-File.open("index.ts", 'w') { |f| f.write(export_string) }
+File.open(FILE_NAME, 'w') { |f| f.write(export_string) }
